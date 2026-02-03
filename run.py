b0719a450b50855a0e29e14748c39da49d5aa0e7
rvnku <artpr.zxc@gmail.com>
2026-02-03 23:18:44 +0500

    simplify startup

--- run.py
+++ run.py
@@ -1,26 +1,8 @@
+from bot.main import start_bot
 import os
-from inputimeout import inputimeout, TimeoutOccurred
-
-TIMEOUT = 3
-
-
-def main():
-    from bot.main import start_bot
-    start_bot()
 
 
 if __name__ == '__main__':
-    print('[!!] Type any char or the console to close after {} sec.'.format(TIMEOUT))
-
-    try:
-        inputimeout(prompt='... ', timeout=TIMEOUT)
-        print('[>_] The console is open (write "exit" to close):')
-    except TimeoutOccurred:
-        os.system('git reset --hard')
-        os.system('git pull origin master')
-    else:
-        while (msg := input('> ')) != 'exit':
-            os.system(msg)
-
-    print('[==] The console is close, starting bot...')
-    main()
+    os.system('git reset --hard')
+    os.system('git pull origin master')
+    start_bot()
